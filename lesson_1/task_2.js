204879f934e6cb1f7d685476988b9ce134d848d8
d.ridniy <allbegj@gmail.com>
2023-08-04 18:25:12 +0500

    home work 1 done

---
+++ lesson_1/task_2.js
@@ -0,0 +1,16 @@
+﻿// 2) Напишите функцию createCounter, которая создает счетчик и возвращает объект с двумя методами: increment и decrement. Метод increment должен увеличивать значение счетчика на 1, а метод decrement должен уменьшать значение счетчика на 1. Значение счетчика должно быть доступно только через методы объекта, а не напрямую.
+
+const createCounter = () => {
+    return {
+        increment(numb) {
+            return ++numb;
+        },
+        decrement(numb) {
+            return --numb;
+        }
+    }
+}
+
+const counter = createCounter();
+console.log(counter.increment(2));
+console.log(counter.decrement(2));
---
+++ lesson_1/task_3.js
@@ -0,0 +1,11 @@
+﻿// 3) Напишем функцию, которая будет находить факториал числа с использованием рекурсии:
+
+const factorial = (numb) => {
+    if (numb === 1) {
+        return numb;
+    } else {
+        return numb * factorial(numb - 1);
+    }
+}
+
+console.log(factorial(5));
