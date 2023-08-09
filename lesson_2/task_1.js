4406f16f69fb7d1e021e63cf90da8c52d2de76c7
d.ridniy <allbegj@gmail.com>
2023-08-09 12:54:44 +0500

    home work 2 done

---
+++ lesson_2/task_1.js
@@ -0,0 +1,21 @@
+﻿// Реализуйте класс Book, представляющий книгу, со следующими свойствами и методами:
+
+// Свойство title (название) - строка, название книги.
+// Свойство author (автор) - строка, имя автора книги.
+// Свойство pages (количество страниц) - число, количество страниц в книге.
+// Метод displayInfo() - выводит информацию о книге (название, автор и количество страниц).
+
+class Book {
+    constructor(title, author, pages) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+    }
+
+    displayInfo() {
+        console.log(this.title, this.author, this.pages);
+    }
+}
+
+const newBook = new Book('Название книги', 'Автор', 45);
+newBook.displayInfo();
---
+++ lesson_2/task_2.js
@@ -0,0 +1,23 @@
+﻿// Задание 2: ""Управление списком студентов""
+// Реализуйте класс Student, представляющий студента, со следующими свойствами и методами:
+
+// Свойство name (имя) - строка, имя студента.
+// Свойство age (возраст) - число, возраст студента.
+// Свойство grade (класс) - строка, класс, в котором учится студент.
+// Метод displayInfo() - выводит информацию о студенте (имя, возраст и класс).
+// javascript
+
+class Student {
+    constructor(name, age, grade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+
+    displayInfo() {
+        console.log(this.name, this.age, this.grade);
+    }
+}
+
+const newStudent = new Student('Имя', '13', '6');
+newStudent.displayInfo();
